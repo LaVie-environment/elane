e2c7dbd4da7cdf737ba927ceeef34dfefca62670
Arm Bun <bellotaophyc@outlook.com>
2022-08-31 19:39:09 +0000

    moove the s3 bucket

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -54,14 +54,3 @@ terraform {
     }
 }
 
-# prints out the Amazon resource name of the S3 bucket
-
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.filling_status.arn
-  description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-    value = aws_dynamodb_table.gap_locks.name
-    description = "The name of the DynamoDB table" 
-}
